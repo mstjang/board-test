2c5af1badc2508adec0d977b6a627bacb17d6200
ws jang <mstjang.com@gmail.com>
2022-08-24 23:42:55 +0900

    #2 - 스프링부트 시작
    * gitignore setting

---
+++ src/test/java/com/mstjang/boardtest/BoardTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mstjang.boardtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
